80ca152f0ed3735a1789bbc7496349435a61c051
nmr <nmrhard@gmail.com>
2021-01-30 21:58:34 +0200

    Build tabbed component

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Pagination
+
 document.querySelector('.nav__links').addEventListener('click', function(evt) {
   evt.preventDefault();
 
@@ -49,3 +51,20 @@ document.querySelector('.nav__links').addEventListener('click', function(evt) {
   }
 })
 
+//Tabs component
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(evt) {
+  const clicked = evt.target.closest('.operations__tab');
+  
+  if(!clicked) return;
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content => content.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+});
