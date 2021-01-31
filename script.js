4977829af33d426e4236e6cf0045b8c42978b483
nmrhard <nmrhard@gmail.com>
2021-01-31 23:11:01 +0200

    Implementing sticky navigation

--- script.js
+++ script.js
@@ -89,3 +89,28 @@ const handleHover = function(evt) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//Sticky navigation
+
+const header = document.querySelector('.header');
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+
+  console.log(entry);
+
+  if (!entry.isIntersecting ) {
+    nav.classList.add('sticky')
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+
+const headerObserver = new IntersectionObserver(
+  stickyNav, {
+    root: null,
+    threshold: 0
+  }
+);
+
+headerObserver.observe(header);
